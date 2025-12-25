afc6728a3f76fed812fb30f8ea3793e1942f5eb3
AlejandroGomez <alejandro.gomez@grupolars.com.mx>
2025-12-25 12:51:38 -0600

    fix calendar seeder

--- database/seeds/CalendarAutoSeeder.php
+++ database/seeds/CalendarAutoSeeder.php
@@ -3,7 +3,7 @@ use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 use Carbon\Carbon;
 
-class CalendarSeeder extends Seeder
+class CalendarAutoSeeder extends Seeder
 {
     public function run(): void
     {
